8b6740ebac4c37a74896d1340fdcafd757f2bb40
nagitausu <nagitaosu.radio@gmail.com>
2019-07-12 19:43:03 +0900

    Fix not initialized bug in seg tree rmq

--- python/rmq.py
+++ python/rmq.py
@@ -11,9 +11,9 @@ class RMQ:
     def initialize(self):
         for i in range(self.n):
             self.data[self.size + i - 1] = a[i]
-        for i in range(self.n-2, -1, -1):
+        for i in range(self.size-2, -1, -1):
             self.data[i] = min(self.data[i*2 + 1], self.data[i*2 + 2])
-    
+
     # Update ak as x
     def update(self, k, x):
         k += self.size - 1
@@ -21,7 +21,7 @@ class RMQ:
         while k > 0:
             k = (k - 1) // 2
             self.data[k] = min(self.data[2*k+1], self.data[2*k+2])
-    
+
     # Min value in [l, r)
     def query(self, l, r):
         L = l + self.size; R = r + self.size
@@ -36,11 +36,16 @@ class RMQ:
             L >>= 1; R >>= 1
         return s
 
-a = [0, 1, 2, 3, 4, 5, 6, 7]
-rmq = RMQ(a)
-print(rmq.data)
-for i in range(8):
-    rmq.update(i, i*2)
-for i in range(8):
-    print(rmq.query(i, 8))
-print(rmq.data)
+if __name__ == "__main__":
+    a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    n = len(a)
+    rmq = RMQ(a)
+    for i in range(n):
+        print(rmq.query(i, n), end=" ")
+    print("")
+    print("update and try agein")
+    for i in range(n):
+        rmq.update(i, i*2)
+    for i in range(n):
+        print(rmq.query(i, n), end=" ")
+    print("")
