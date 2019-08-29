580d5f31f4887f0a8224f94bfd641a38431e89dc
nagitausu <nagitaosu.radio@gmail.com>
2019-08-29 12:43:54 +0900

    Fix i,j,k disorder bug

--- python/warshall-froyd.py
+++ python/warshall-froyd.py
@@ -1,16 +1,21 @@
 n = 5
 INF = 10**5
-ngraph = [[INF] * n for _ in range(n)]
-graph = [[0,21,18,11,28],
-         [21,0,13,10,26],
-         [18,13,0,23,13],
-         [11,10,23,0,17],
-         [28,26,13,17,0]]
-        
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            ngraph[i][j] = min(ngraph[i][j], graph[i][k] + graph[k][j])
+dist = [[INF] * n for _ in range(n)]
+graph = [[0,21,2,11,39],
+         [21,0,INF,10,INF],
+         [2,INF,0,23,INF],
+         [11,10,23,0,INF],
+         [39,INF,INF,INF,0]]
 
-for item in ngraph:
-    print(item)
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            dist[i][j] = min(dist[i][j], graph[i][k] + graph[k][j])
+
+print("graph")
+for line in graph:
+    print(line)
+
+print("dist")
+for line in dist:
+    print(line)
