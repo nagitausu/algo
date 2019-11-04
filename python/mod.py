181cc9bac50df5e5c6f0928332218b6278b90fce
nagitausu <nagitaosu.radio@gmail.com>
2019-11-04 12:42:42 +0900

    Not to pow for n times while getting mod inverse

--- python/mod.py
+++ python/mod.py
@@ -2,14 +2,17 @@ MOD = 10**9 + 7
 MAX_N = 10**5 
 
 fac = [1] + [0] * MAX_N
-fac_inv = [1] + [0] * MAX_N
 for i in range(1, MAX_N+1):
     fac[i] = fac[i-1] * (i) % MOD
-    # Fermat's little theorem says
-    # a**(p-1) mod p == 1
-    # then, a * a**(p-2) mod p == 1
-    # it means a**(p-2) is inverse element
-    fac_inv[i] = fac_inv[i-1] * pow(i, MOD-2, MOD) % MOD
+
+fac_inv = [1] + [0] * MAX_N
+# Fmrmat's little theorem says a**(p-1) mod p == 1
+# then, a * a**(p-2) mod p == 1
+# it means that a**(p-2) is the inverse element
+# Here, Get 1 / n! first
+fac_inv[MAX_N] = pow(fac[MAX_N], MOD-2, MOD)
+for i in range(MAX_N, 1, -1):
+    fac_inv[i-1] = fac_inv[i] * i % MOD
 
 def mod_nCr(n, r):
     if n < r or n < 0 or r < 0:
