c3427f894882eacd416e96a28ec63bd7e6cec7f5
nagitausu <nagitaosu.radio@gmail.com>
2019-11-07 18:47:34 +0900

    Enable generation of graph with cost

--- util/graph_generator.py
+++ util/graph_generator.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import random
 
-def generate_tree(n, raw=False):
+def generate_tree(n, raw=False, cost=False):
     nodes = [i for i in range(n)]
     random.shuffle(nodes)
 
@@ -15,16 +15,25 @@ def generate_tree(n, raw=False):
         v = nodes[i]
         if u > v:
             u, v = v, u
-        tree_edge.append((u, v))
+        if cost:
+            c = random.randrange(1, n+1)
+            tree_edge.append((u, v, c))
+        else:
+            tree_edge.append((u, v))
         group.append(nodes[i])
 
     if raw:
         return tree_edge
     else:
         tree_edge_list = [[] for _ in range(n)]
-        for u, v in tree_edge:
-            tree_edge_list[u].append(v)
-            tree_edge_list[v].append(u)
+        if cost:
+          for u, v, c in tree_edge:
+              tree_edge_list[u].append((v, c))
+              tree_edge_list[v].append((u, c))
+        else:
+          for u, v in tree_edge:
+              tree_edge_list[u].append(v)
+              tree_edge_list[v].append(u)
         return tree_edge_list
 
 def generate_simple_connected_graph(n, density=1, raw=False):
