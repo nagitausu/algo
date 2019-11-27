1a02d2e4485ae6382e86816552c8bfdb353b5498
nagitausu <nagitaosu.radio@gmail.com>
2019-11-27 19:33:41 +0900

    Abstract range min/max query segment tree

--- python/rmq.py
+++ python/rmq.py
@@ -1,10 +1,12 @@
-INF = 10**9
-
 class RMQ:
     def __init__(self, a):
+        # Operator
+        self.op = lambda a, b : min(a, b)
+        # Identity element
+        self.e = 10**9 
         self.n = len(a)
         self.size = 2**(self.n - 1).bit_length()
-        self.data = [INF] * (2*self.size-1)
+        self.data = [self.e] * (2*self.size-1)
         self.initialize(a)
 
     # Initialize data
@@ -12,32 +14,32 @@ class RMQ:
         for i in range(self.n):
             self.data[self.size + i - 1] = a[i]
         for i in range(self.size-2, -1, -1):
-            self.data[i] = min(self.data[i*2 + 1], self.data[i*2 + 2])
+            self.data[i] = self.op(self.data[i*2 + 1], self.data[i*2 + 2])
 
-    # Update ak as x
+    # Update ak as x (0-indexed)
     def update(self, k, x):
         k += self.size - 1
         self.data[k] = x
         while k > 0:
             k = (k - 1) // 2
-            self.data[k] = min(self.data[2*k+1], self.data[2*k+2])
+            self.data[k] = self.op(self.data[2*k+1], self.data[2*k+2])
 
-    # Min value in [l, r)
+    # Min value in [l, r) (0-indexed)
     def query(self, l, r):
         L = l + self.size; R = r + self.size
-        s = INF
+        s = self.e
         while L < R:
             if R & 1:
                 R -= 1
-                s = min(s, self.data[R-1])
+                s = self.op(s, self.data[R-1])
             if L & 1:
-                s = min(s, self.data[L-1])
+                s = self.op(s, self.data[L-1])
                 L += 1
             L >>= 1; R >>= 1
         return s
 
 if __name__ == "__main__":
-    b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    b = [8, 3, 2, 6, 9, 11, 2, 7, 0, 2]
     n = len(b)
     rmq = RMQ(b)
     for i in range(n):
