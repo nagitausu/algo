ba666df3e37c8f90cffcdea6ed701e4b790c8256
nagitausu <nagitaosu.radio@gmail.com>
2020-01-17 20:25:07 +0900

    Add bisect on binary indexed tree

--- python/binary_indexed_tree.py
+++ python/binary_indexed_tree.py
@@ -1,32 +1,52 @@
-n = 16 
-bit = [0] * (n + 1) 
+class BinaryIndexedTree:
+    # Initialize BIT with non-zero value
+    def __init__(self, array):
+        self.len = len(array)
+        self.data = [0] + array
+        self.build()
 
-# Initialize BIT with non-zero value
-def bit_init():
-    for i in range(1, n):
-        if i + (i & -i) > n:
-            continue
-        bit[i + (i & -i)] += bit[i]
+    def build(self):
+        for i in range(1, self.len):
+            if i + (i & -i) > self.len:
+                continue
+            self.data[i + (i & -i)] += self.data[i]
 
-# Add w to a[x] (0-indexed) 
-def bit_add(x, w):
-    x += 1
-    while x <= n:
-        bit[x] += w
-        x += x & -x
+    # Add w to a[x] (0-indexed)
+    def add(self, x, w):
+        x += 1
+        while x <= self.len:
+            self.data[x] += w
+            x += x & -x
 
-# Get Sum of a[0 : x) (0-indexed) 
-def bit_sum(x):
-    ret = 0
-    while x > 0:
-        ret += bit[x]
-        x -= x & -x
-    return ret
+    # Get Sum of a[0 : x) (0-indexed)
+    def sum(self, x):
+        ret = 0
+        while x > 0:
+            ret += self.data[x]
+            x -= x & -x
+        return ret
 
-bit_init()
-print(bit[1:])
-for i in range(0, n):
-    bit_add(i, 1)
-print(bit[1:])
-for i in range(1,n+1):
-    print(bit_sum(i))
+    # Get first index s.t. sum a[0 : index) >= w
+    def bisect_left(self, w):
+        if w <= 0:
+            return 0
+        x = 0
+        delta = pow(2, (self.len - 1).bit_length())
+        while delta > 0:
+            if x + delta <= self.len and self.data[x + delta] < w:
+                w -= self.data[x + delta]
+                x += delta
+            delta >>= 1
+        return x
+
+if __name__ == "__main__":
+    array = [item + 1 for item in range(16)]
+    n = len(array)
+    BIT = BinaryIndexedTree(array)
+    for i in range(0, n):
+        BIT.add(i, i)
+    cumsum = []
+    for i in range(1, n+1):
+        cumsum.append(BIT.sum(i))
+    print(cumsum)
+    print(BIT.bisect_left(25))
