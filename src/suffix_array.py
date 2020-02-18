005048e2d87d0849f22c040649650a2b9f3d77f0
nagitausu <nagitaosu.radio@gmail.com>
2020-02-18 16:49:34 +0900

    Fix bug in "bab" and "abbba" case

--- src/suffix_array.py
+++ src/suffix_array.py
@@ -3,8 +3,7 @@ class SuffixArray:
         self.a = a + [0]
         self.k = max(self.a) + 1
         self.n = len(self.a)
-        self.sa = None
-        self.sa = self.SA_IS(self.a, self.sa, self.n, self.k)
+        self.sa = self.SA_IS(self.a, self.n, self.k)
 
     # Find the start or end of each bucket
     def get_buckets(self, a, k, end=True):
@@ -20,112 +19,127 @@ class SuffixArray:
                 bucket[i] = sumation - bucket[i]
         return bucket
 
-    def induce_SAl(self, a, sa, n, k, is_stype):
+    def induce_SAl(self, a, sa, n, k, lstype):
         bucket = self.get_buckets(a, k, end=False)
         for i in range(n):
             j = sa[i] - 1
-            if j >= 0 and (not is_stype & (1 << j)):
+            if j >= 0 and (not lstype & (1 << j + 1)):
                 sa[bucket[a[j]]] = j
                 bucket[a[j]] += 1
 
-    def induce_SAs(self, a, sa, n, k, is_stype):
+    def induce_SAs(self, a, sa, n, k, lstype):
         bucket = self.get_buckets(a, k, end=True)
         for i in range(n-1, -1, -1):
             j = sa[i] - 1
-            if j >= 0 and is_stype & (1 << j):
+            if j >= 0 and lstype & (1 << j + 1):
                 bucket[a[j]] -= 1
                 sa[bucket[a[j]]] = j
+    
+    def is_LMS(self, index, lstype):
+        return lstype >> (index+1) & 1 and not (lstype >> index & 1)
 
-    def SA_IS(self, a, sa, n, k):
-        # LS-type array in bits
-        is_stype = 0
+    def is_stype(self, index, lstype):
+        return lstype >> (index+1) & 1
+
+    def SA_IS(self, a, n, k):
+        # L-type or S-type
+        # LS-type array in bits, 1-indexed, initialized with S-type first
+        lstype = 1
         prev_type = 0
-        prev_item = self.k 
+        prev_item = self.k
         for i, item in enumerate(a[::-1]): 
-            shift = n - 1 - i
+            shift = n - i
             if item == prev_item:
-                is_stype |= prev_type << shift 
+                lstype |= prev_type << shift 
             elif item < prev_item:
-                is_stype |= 1 << shift 
-            prev_type = item <= prev_item
+                lstype |= 1 << shift 
+                prev_type = 1
+            else:
+                prev_type = 0
             prev_item = item
         # Bucket array
         bucket = self.get_buckets(a, k)
         # Find ends of buckets
         sa = [-1] * n
         for i in range(n - 1, 0, -1):
-            is_LMS = is_stype & (1 << i) and (not is_stype & (1 << i-1))
-            if is_LMS:
+            if self.is_LMS(i, lstype):
                 bucket[a[i]] -= 1
                 sa[bucket[a[i]]] = i
-        self.induce_SAl(a, sa, n, k, is_stype)
-        self.induce_SAs(a, sa, n, k, is_stype)
+        self.induce_SAl(a, sa, n, k, lstype)
+        self.induce_SAs(a, sa, n, k, lstype)
 
-        n1 = 0
+        lcm_cnt = 0
         for i in range(n):
-            if sa[i] == 0: continue
-            is_LMS = is_stype & (1 << sa[i]) and (not is_stype & (1 << sa[i]-1))
-            if is_LMS:
-                sa[n1] = sa[i]
-                n1 += 1
-        for i in range(n1, n):
+            if self.is_LMS(sa[i], lstype):
+                sa[lcm_cnt] = sa[i]
+                lcm_cnt += 1
+        for i in range(lcm_cnt, n):
             sa[i] = -1
 
-        name = 1; prev = 0 
-        names = [0]
-        sa[n1] = 0
-        for pos in sa[1: n1]:
-            if pos == 0:
-                continue
+        # Rename LCM strings
+        name = 1; prev = sa[0] 
+        sa[lcm_cnt + sa[0] // 2] = 0
+        for pos in sa[1: lcm_cnt]:
             diff = False
             for d in range(n):
-                is_LMS1 = is_stype & (1 << pos+d) and (not is_stype & (1 << pos+d-1))
-                is_LMS2 = is_stype & (1 << prev+d) and (not is_stype & (1 << prev+d-1))
-                if a[pos + d] != a[prev+d] or (is_stype >> (pos + d) & 1) != (is_stype >> (prev + d) & 1):
+                if a[pos + d] != a[prev + d] or (self.is_stype(pos + d, lstype) != self.is_stype(prev + d, lstype)):
                     diff = True
                     break
-                elif d > 0 and (is_LMS1 or is_LMS2):
+                elif d > 0 and (self.is_LMS(pos + d, lstype) or self.is_LMS(prev + d, lstype)):
                     break
             if diff:
                 name += 1; prev = pos
             pos //= 2
-            sa[n1 + pos] = name - 1
-            names.append(name - 1)
-
-        j = n - 1
-        for i in range(n-1, n1 - 1, -1):
+            sa[lcm_cnt + pos] = name - 1
+        sub_a = []
+        for i in range(lcm_cnt, n):
             if sa[i] >= 0:
-                sa[j] = sa[i]
-                j -= 1
-        sa1 = sa[:n1]; a1 = sa[-n1:] 
-        if name < n1:
-            sa1 = self.SA_IS(a1, sa1, n1, name)
+                sub_a.append(sa[i])
+            
+        # Recursively sort LMS sub strings
+        if name < lcm_cnt:
+            sub_sa = self.SA_IS(sub_a, len(sub_a), name + 1)
         else:
-            for i in range(n1):
-                sa1[a1[i]] = i
+            sub_sa = sa[:lcm_cnt]
+            for i in range(lcm_cnt):
+                sub_sa[sub_a[i]] = i
         bucket = self.get_buckets(a, k, end=True)
         j = 0
-        for i in range(n):
-            if i <= 0: continue
-            is_LMS = is_stype & (1 << i) and (not is_stype & (1 << i-1))
-            if is_LMS:
-                a1[j] = i
+        for i in range(1, n):
+            if self.is_LMS(i, lstype):
+                sub_a[j] = i
                 j += 1
-        for i in range(n1):
-            sa1[i] = a1[sa1[i]]
-        for i in range(n1, n):
+        # Earn correctly sorted LCMs
+        for i in range(lcm_cnt):
+            sa[i] = sub_a[sub_sa[i]]
+        for i in range(lcm_cnt, n):
             sa[i] = -1
-        for i in range(n1 - 1, -1, -1):
+        for i in range(lcm_cnt - 1, -1, -1):
             j = sa[i]; sa[i] = -1
             bucket[a[j]] -= 1
             sa[bucket[a[j]]] = j
-        self.induce_SAl(a, sa, n, k, is_stype)
-        self.induce_SAs(a, sa, n, k, is_stype)
+
+        # Correct induced sorting
+        self.induce_SAl(a, sa, n, k, lstype)
+        self.induce_SAs(a, sa, n, k, lstype)
         return sa
 
 if __name__ == "__main__":
-    s = "mmiissiissiippii"
+    s = "honifuwadijkstrababababa"
     ord_base = ord("a") - 1
     a = [ord(ch) - ord_base for ch in s]
     SA = SuffixArray(a)
     print(SA.sa)
+
+    sa_naive = [""]
+    for i in range(len(s)):
+        sa_naive.append(s[i:])
+    sa_naive.sort()
+    ok = True
+    for i, index in enumerate(SA.sa):
+        print(s[index:], sa_naive[i])
+        if s[index:] != sa_naive[i]:
+            print("wrong!")
+            ok = False
+    if ok:
+        print("\nverified!")
